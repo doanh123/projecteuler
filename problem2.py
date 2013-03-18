5e870bb01a4dcfac932c84e8718efb2c0a1f1d7f
doanh123 <doanh@intellievents.com>
2013-03-18 07:40:35 -0700

    recent change

--- problem2.py
+++ problem2.py
@@ -1,1 +1,17 @@
-sfsfs
+#By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
+from fibonaccigenerator import fib
+
+def foo():
+   sum = 0
+   a = fib()
+   while True:
+     b=a.next()
+     if b>4000000:
+        break
+     if b%2==0:
+        sum +=b
+
+   print sum
+
+foo()
+#ans: 4613732   
